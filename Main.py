ae664dbdabf9c9223dc5b5ad1db443ee8057d8dc
Daniel Howard <acrodemocide@hotmail.com>
2019-07-09 06:47:53 -0600

    Made market member private on stockMarket

--- Main.py
+++ Main.py
@@ -7,9 +7,13 @@ appleStock = stockMarket.GetStock('AAPL')
 microsoftStock = stockMarket.GetStock('MSFT')
 googleStock = stockMarket.GetStock('GOOGL')
 
+watchedStocks = []
+watchedStocks.append(appleStock)
+watchedStocks.append(microsoftStock)
+watchedStocks.append(googleStock)
+
 print('On Volume Balance Analysis')
-for stockSymbol in stockMarket.market:
-    stock = stockMarket.market[stockSymbol]
+for stock in watchedStocks:
     if ShouldBuy(stock):
         print('Buy ' + stock.name)
     elif ShouldSell(stock):
--- StockMarket.py
+++ StockMarket.py
@@ -2,10 +2,10 @@ from Stock import Stock
 
 class StockMarket:
     def __init__(self):
-        self.market = {}
-        self.market['AAPL'] = Stock('Apple', 'AAPL')
-        self.market['MSFT'] = Stock('Microsoft', 'MSFT')
-        self.market['GOOGL'] = Stock('Google', "GOOGL")
+        self.__market = {}
+        self.__market['AAPL'] = Stock('Apple', 'AAPL')
+        self.__market['MSFT'] = Stock('Microsoft', 'MSFT')
+        self.__market['GOOGL'] = Stock('Google', "GOOGL")
     
     def GetStock(self, symbol):
-        return self.market[symbol]
+        return self.__market[symbol]
