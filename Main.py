5e210eddcad087c061ae979ff0ab083bd1834344
Daniel Howard <acrodemocide@hotmail.com>
2019-07-09 06:37:16 -0600

    Moved stock market into its own class

--- Main.py
+++ Main.py
@@ -7,21 +7,12 @@ appleStock = stockMarket.GetStock('AAPL')
 microsoftStock = stockMarket.GetStock('MSFT')
 googleStock = stockMarket.GetStock('GOOGL')
 
-currentlyHeldStock = []
-currentlyHeldStock.append(appleStock)
-currentlyHeldStock.append(microsoftStock)
-currentlyHeldStock.append(googleStock)
-
 print('On Volume Balance Analysis')
 for stockSymbol in stockMarket.market:
     stock = stockMarket.market[stockSymbol]
     if OnBalanceVolumeShouldBuy(stock):
         print('Buy ' + stock.name)
+    elif OnBalanceVolumeShouldSell(stock):
+        print('Sell ' + stock.name)
     else:
-        print('Do not buy ' + stock.name)
-
-for stockSymbol in currentlyHeldStock:
-    if OnBalanceVolumeShouldSell(stockSymbol):
-        print('Sell ' + stockSymbol.name)
-    else:
-        print('Hold ' + stockSymbol.name)
+        print('Hold ' + stock.name)
