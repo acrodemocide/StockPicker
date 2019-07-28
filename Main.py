875d808ac31fa2f2b20fbf6877d87433fe1e37cb
Daniel Howard <acrodemocide@hotmail.com>
2019-07-28 10:02:20 -0600

    Added functionality to create onBalanceVolume array to evaluate the stock

--- Main.py
+++ Main.py
@@ -5,27 +5,10 @@ from StockMarket import StockMarket
 
 stockMarket = StockMarket()
 appleStock = stockMarket.GetStock('AAPL')
-# microsoftStock = stockMarket.GetStock('MSFT')
-# googleStock = stockMarket.GetStock('GOOGL')
-
-# watchedStocks = []
-# watchedStocks.append(appleStock)
-# watchedStocks.append(microsoftStock)
-# watchedStocks.append(googleStock)
 
 # print(appleStock.dailyClose)
 
-accumulatedOnBalanceVolume = OnBalanceVolumeOverTime(appleStock)
-print(accumulatedOnBalanceVolume)
+# accumulatedOnBalanceVolume = OnBalanceVolumeOverTime(appleStock)
+# print(accumulatedOnBalanceVolume)
 
-# print('On Volume Balance Analysis')
-# for stock in watchedStocks:
-#     if ShouldBuy(stock):
-#         print('Buy ' + stock.name)
-#     elif ShouldSell(stock):
-#         print('Sell ' + stock.name)
-#     else:
-#         print('Hold ' + stock.name)
-    # with open('data.txt', 'w') as outfile:
-    #     json.dump(data, outfile)
-# print(OnBalanceVolumeApi(stock))
+print(appleStock.onBalanceVolume)
--- Stock.py
+++ Stock.py
@@ -6,37 +6,47 @@ class Stock:
     def __init__(self, name, symbol):
         self.name = name
         self.symbol = symbol
+        self.dates = []
         self.dailyHigh = []
         self.dailyLow = []
         self.dailyOpen = []
         self.dailyClose = []
+        self.adjustedClose = []
         self.volume = []
+        self.onBalanceVolume = []
         
         with open('AAPL.csv') as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
             line_count = 0
             for row in csv_reader:
+                currentDate = row[0]
+                currentOpen = row[1]
+                currentHigh = row[2]
+                currentLow = row[3]
+                currentClose = row[4]
+                currentAdjustedClose = row[5]
+                currentVolume = row[6]
                 if line_count > 0:
-                    # print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-                    self.dailyOpen.append(row[1])
-                    self.dailyHigh.append(row[2])
-                    self.dailyLow.append(row[3])
-                    self.dailyClose.append(row[4])
-                    self.volume.append(row[6])
-                line_count += 1
+                    self.dates.append(currentDate)
+                    self.dailyOpen.append(currentOpen)
+                    self.dailyHigh.append(currentHigh)
+                    self.dailyLow.append(currentLow)
+                    self.dailyClose.append(currentClose)
+                    self.adjustedClose.append(currentAdjustedClose)
+                    self.volume.append(currentVolume)
+                if (line_count == 1):
+                    self.onBalanceVolume.append(0)
+                elif(line_count > 1):
+                    previousDayIndex = line_count - 2
+                    currentDayIndex = line_count - 1
+                    previousDayClosingPrice = float(self.dailyClose[previousDayIndex])
+                    currentDayClosingPrice = float(self.dailyClose[currentDayIndex])
+                    previousDayVolume = int(self.volume[previousDayIndex])
+                    currentDayVolume = int(self.volume[currentDayIndex])
 
-    # def GenerateDummyData(self):
-    #     lowPossibility = 0
-    #     highPossibility = 100
-    #     numberOfValues = 100
-    #     self.dailyHigh = self.GenerateRandomValues(lowPossibility, highPossibility, numberOfValues)
-    #     self.dailyLow = self.GenerateRandomValues(lowPossibility, highPossibility, numberOfValues)
-    #     self.dailyOpen = self.GenerateRandomValues(lowPossibility, highPossibility, numberOfValues)
-    #     self.dailyClose = self.GenerateRandomValues(lowPossibility, highPossibility, numberOfValues)
-    #     self.volume = self.GenerateRandomValues(lowPossibility, highPossibility, numberOfValues)
-    
-    # def GenerateRandomValues(self, lowPossibility, highPossibility, numberOfValues):
-    #     randomGeneratedValues = []
-    #     for x in range(numberOfValues):
-    #         randomGeneratedValues.append(random.randint(lowPossibility, highPossibility))
-    #     return randomGeneratedValues
+                    closingPriceChange = currentDayClosingPrice - previousDayClosingPrice
+                    onBalanceVolumeFactor = -1 if closingPriceChange < 0 else 1 if closingPriceChange > 0 else 0
+                    volumeToAdd = currentDayVolume * onBalanceVolumeFactor
+
+                    self.onBalanceVolume.append(previousDayVolume + volumeToAdd)
+                line_count += 1
