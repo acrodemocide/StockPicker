8f04c768dbb33a039963ffe5e6481ff7509209df
Daniel Howard <acrodemocide@hotmail.com>
2019-08-10 14:25:58 -0600

    Removed unneeded code

--- Data/Stock.py
+++ Data/Stock.py
@@ -9,23 +9,3 @@ class Stock:
         self.adjustedClose = []
         self.volume = []
         self.onBalanceVolume = []
-        
-    # Tech Debt -- We need to create a way where we get this information
-    #   from the repository rather than just leaving it here
-    def CopyToDate(self, date):
-        totalNumberOfQuotes = len(self.dates)
-        indexOfDesiredDate = 0
-        while self.dates[indexOfDesiredDate] < date and indexOfDesiredDate < totalNumberOfQuotes:    
-            indexOfDesiredDate += 1
-
-        copyStock = Stock(self.symbol)
-        copyStock.dates = self.dates[0:indexOfDesiredDate]
-        copyStock.dailyOpen = self.dailyOpen[0:indexOfDesiredDate]
-        copyStock.dailyHigh = self.dailyHigh[0:indexOfDesiredDate]
-        copyStock.dailyLow = self.dailyLow[0:indexOfDesiredDate]
-        copyStock.dailyClose = self.dailyClose[0:indexOfDesiredDate]
-        copyStock.adjustedClose = self.adjustedClose[0:indexOfDesiredDate]
-        copyStock.volume = self.volume[0:indexOfDesiredDate]
-        copyStock.onBalanceVolume = self.onBalanceVolume[0:indexOfDesiredDate]
-
-        return copyStock
