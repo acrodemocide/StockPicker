31314b1558450d4b6656866c943f858e6e0c5bb1
Daniel Howard <acrodemocide@hotmail.com>
2019-08-10 13:42:27 -0600

    Added the rest of the unit tests needed for Portfolio.py

--- Tests/PortfolioTests.py
+++ Tests/PortfolioTests.py
@@ -8,7 +8,7 @@ from Data.StockRepository import BackTestStockRepository
 # Pytest requires "test_" prefix to consider a function a test to run
 class PortfolioTests(unittest.TestCase):
 
-    def setUp(self): 
+    def setUp(self):
         pass
     
     def test_GetTotalValue_InitialSetup(self):
@@ -104,6 +104,57 @@ class PortfolioTests(unittest.TestCase):
         # Assert
         actualValue = portfolio.GetTotalValue()
         self.assertEqual(actualValue, expectedPortfolioValue)
+        self.assertEqual(expectedStockHeldQuantity, portfolio.heldStocks['AAPL'])
+    
+    def test_SellAllStocks(self):
+        # Arrange
+        mockStockRepository = BackTestStockRepository()
+        mockStockRepository.GetCurrentStockPrice = MagicMock(return_value = 300)
+        startingCash = 1000
+        portfolio = Portfolio(startingCash, mockStockRepository)
+        portfolio.heldStocks['AAPL'] = 10
+        numberOfStocksToSell = 10
+        expectedStockHeldQuantity = 0
+        expectedPortfolioValue = 4000
+        # Act
+        portfolio.Sell('AAPL', numberOfStocksToSell)
+        # Assert
+        actualValue = portfolio.GetTotalValue()
+        self.assertEqual(expectedPortfolioValue, actualValue)
+        self.assertEqual(expectedStockHeldQuantity, portfolio.heldStocks['AAPL'])
+
+    def test_SellAllStocks_SellOrderGreaterThanHeldQuantity(self):
+        # Arrange
+        mockStockRepository = BackTestStockRepository()
+        mockStockRepository.GetCurrentStockPrice = MagicMock(return_value = 300)
+        startingCash = 1000
+        portfolio = Portfolio(startingCash, mockStockRepository)
+        portfolio.heldStocks['AAPL'] = 10
+        numberOfStocksToSell = 11
+        expectedStockHeldQuantity = 0
+        expectedPortfolioValue = 4000
+        # Act
+        portfolio.Sell('AAPL', numberOfStocksToSell)
+        # Assert
+        actualValue = portfolio.GetTotalValue()
+        self.assertEqual(expectedPortfolioValue, actualValue)
+        self.assertEqual(expectedStockHeldQuantity, portfolio.heldStocks['AAPL'])
+    
+    def test_SellAllStocks_SellOrderLessThanHeldQuantity(self):
+        # Arrange
+        mockStockRepository = BackTestStockRepository()
+        mockStockRepository.GetCurrentStockPrice = MagicMock(return_value = 300)
+        startingCash = 1000
+        portfolio = Portfolio(startingCash, mockStockRepository)
+        portfolio.heldStocks['AAPL'] = 10
+        numberOfStocksToSell = 5
+        expectedStockHeldQuantity = 5
+        expectedPortfolioValue = 4000
+        # Act
+        portfolio.Sell('AAPL', numberOfStocksToSell)
+        # Assert
+        actualValue = portfolio.GetTotalValue()
+        self.assertEqual(expectedPortfolioValue, actualValue)
         self.assertEqual(expectedStockHeldQuantity, portfolio.heldStocks['AAPL'])
     
 if __name__ == '__main__': 
