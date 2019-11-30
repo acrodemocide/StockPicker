3e6e342aa9b4adfcbb203d2171d6dffaf226034c
Daniel Howard <acrodemocide@hotmail.com>
2019-11-29 17:05:40 -0700

    Made notes about unit testing main module in python. Added calculation for finding ROI on trading algorithm

--- Main.py
+++ Main.py
@@ -1,14 +1,29 @@
 import sys
 sys.path.append('./TradingAlgorithms/')
-from OnBalanceVolume import obv
 from Data.Stock import Stock
 from Data.StockRepository import StockRepository
 from BackTester import BackTest
+from OnBalanceVolume import obv
+
+# We should probably move the below function into a separate module that
+#   provides utilities that we need.
+def CalculateROI(portfolioValues):
+    roi = 0.0
+    if (len(portfolioValues) > 1):
+        startingValue = portfolioValues[0]
+        endingValue = portfolioValues[len(portfolioValues) - 1]
+        difference = endingValue - startingValue
+        roi = difference / startingValue
+    return roi
 
 startDate = ''
 endDate = ''
 portfolioValues = BackTest(obv, startDate, endDate)
 print(portfolioValues)
+
+roi = CalculateROI(portfolioValues)
+print('Ending ROI: ')
+print(roi)
 
 # print('--------------------------------------')
 # print('--------------------------------------')
--- Tests/UnitTests/MainUnitTests.py
+++ Tests/UnitTests/MainUnitTests.py
@@ -4,6 +4,10 @@ sys.path.append('../')
 # from TradingAlgorithms.OnBalanceVolume import OnBalanceVolumeOverTime, OnBalanceVolume
 # from Data.Stock import Stock
 
+# It seems that there is no clear way to unit test the python main module,
+#   so it should be kept logically simple enough that no unit tests are
+#   needed to test it. We can write an automated test to test the main
+#   method if it is essential that we do so.
 class MainUnitTests(unittest.TestCase):
     def setUp(self):
         pass
